dcde9ff58e48efd2876b49a05a475a7485d7b893
betocoutinho <robertogmcfilho@gmail.com>
2023-06-07 22:14:49 -0300

    Tela inicial pronta

--- app/src/main/java/com/example/shoppingcartv1/MainActivity.java
+++ app/src/main/java/com/example/shoppingcartv1/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.shoppingcartv1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_cliente, bt_produto, bt_venda;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_cliente = findViewById(R.id.bt_cliente);
+        bt_produto = findViewById(R.id.bt_produto);
+        bt_venda = findViewById(R.id.bt_venda);
+
+        bt_cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        bt_produto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        bt_venda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
